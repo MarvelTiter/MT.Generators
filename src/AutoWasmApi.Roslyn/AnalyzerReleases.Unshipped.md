d16caf7c0916c63f55a855920adeaab6599fb7e4
ihonliu <ihonliu@gmail.com>
2024-12-06 16:52:05 +0800

    feat: 添加Analyzer监测追踪

---
+++ src/AutoWasmApi.Roslyn/AnalyzerReleases.Unshipped.md
@@ -0,0 +1,15 @@
+; Unshipped analyzer release
+; https://github.com/dotnet/roslyn-analyzers/blob/master/src/Microsoft.CodeAnalysis.Analyzers/ReleaseTrackingAnalyzers.Help.md
+
+### New Rules
+Rule ID | Category | Severity | Notes
+--------|----------|----------|-------
+WAG00001 | AutoWasmApiGenerator.ControllerGenerator | Error | 继承多个接口需要指定接口标注[WebControllerAttribute]
+WAG00002 | AutoWasmApiGenerator.HttpServiceInvokerGenerator | Error | 无法为该类型生成WebApi调用类，缺少接口
+WAG00003 | AutoWasmApiGenerator.HttpServiceInvokerGenerator | Error | 方法参数过多
+WAG00004 | AutoWasmApiGenerator.HttpServiceInvokerGenerator | Error | 控制器（controller）不能包含泛型
+WAG00005 | AutoWasmApiGenerator.HttpServiceInvokerGenerator | Error | 仅支持异步方法
+WAG00006 | AutoWasmApiGenerator.HttpServiceInvokerGenerator | Error | 路由中未包含路由参数({0})
+WAG00007 | AutoWasmApiGenerator.HttpServiceInvokerGenerator | Error | 不能同时设置[FromBody]和[FromForm]({0})
+WAG00008 | AutoWasmApiGenerator.HttpServiceInvokerGenerator | Error | 不能设置多个[FromBody]({0})
+WAG00009 | AutoWasmApiGenerator.HttpServiceInvokerGenerator | Error | 暂不支持的返回值类型({0})
--- src/AutoWasmApi.Roslyn/DiagnosticDefinitions.cs
+++ src/AutoWasmApi.Roslyn/DiagnosticDefinitions.cs
@@ -7,121 +7,122 @@ namespace AutoWasmApiGenerator
 {
     internal class DiagnosticDefinitions
     {
-        /// <summary>
-        /// 继承多个接口需要指定接口标注[WebControllerAttribute]
-        /// </summary>
-        /// <param name="location"></param>
-        /// <returns></returns>
-        public static Diagnostic WAG00001(Location? location) => Diagnostic.Create(new DiagnosticDescriptor(
-                        id: "WAG00001",
-                        title: "继承多个接口需要指定接口标注[WebControllerAttribute]",
-                        messageFormat: "继承多个接口需要指定接口标注[WebControllerAttribute]",
-                        category: typeof(ControllerGenerator).FullName!,
-                        defaultSeverity: DiagnosticSeverity.Error,
-                        isEnabledByDefault: true), location);
+        private static readonly DiagnosticDescriptor Wag00001DiagDescriptor = new DiagnosticDescriptor(
+                id: "WAG00001",
+                title: "继承多个接口需要指定接口标注[WebControllerAttribute]",
+                messageFormat: "继承多个接口需要指定接口标注[WebControllerAttribute]",
+                category: typeof(ControllerGenerator).FullName!,
+                defaultSeverity: DiagnosticSeverity.Error,
+                isEnabledByDefault: true);
 
-        /// <summary>
-        /// 无法为该类型生成WebApi调用类，缺少接口
-        /// </summary>
-        /// <param name="location"></param>
-        /// <returns></returns>
-        public static Diagnostic WAG00002(Location? location) => Diagnostic.Create(new DiagnosticDescriptor(
-                        id: "WAG00002",
-                        title: "无法为该类型生成WebApi调用类，缺少接口",
-                        messageFormat: "无法为该类型生成WebApi调用类，缺少接口",
-                        category: typeof(HttpServiceInvokerGenerator).FullName!,
-                        defaultSeverity: DiagnosticSeverity.Error,
-                        isEnabledByDefault: true), location);
+        private static readonly DiagnosticDescriptor Wag00002DiagDescriptor = new DiagnosticDescriptor(
+                id: "WAG00002",
+                title: "无法为该类型生成WebApi调用类，缺少接口",
+                messageFormat: "无法为该类型生成WebApi调用类，缺少接口",
+                category: typeof(HttpServiceInvokerGenerator).FullName!,
+                defaultSeverity: DiagnosticSeverity.Error,
+                isEnabledByDefault: true);
 
-        /// <summary>
-        /// 方法参数过多
-        /// </summary>
-        /// <param name="location"></param>
-        /// <returns></returns>
-        public static Diagnostic WAG00003(Location? location) => Diagnostic.Create(new DiagnosticDescriptor(
-                        id: "WAG00003",
-                        title: "方法参数过多",
-                        messageFormat: "方法参数过多",
-                        category: typeof(HttpServiceInvokerGenerator).FullName!,
-                        defaultSeverity: DiagnosticSeverity.Error,
-                        isEnabledByDefault: true), location);
+        private static readonly DiagnosticDescriptor Wag00003DiagDescriptor = new DiagnosticDescriptor(
+                id: "WAG00003",
+                title: "方法参数过多",
+                messageFormat: "方法参数过多",
+                category: typeof(HttpServiceInvokerGenerator).FullName!,
+                defaultSeverity: DiagnosticSeverity.Error,
+                isEnabledByDefault: true);
 
-        /// <summary>
-        /// 控制器（controller）不能包含泛型
-        /// </summary>
-        /// <param name="location"></param>
-        /// <returns></returns>
-        public static Diagnostic WAG00004(Location? location) => Diagnostic.Create(new DiagnosticDescriptor(
-                        id: "WAG00004",
-                        title: "控制器（controller）不能包含泛型",
-                        messageFormat: "控制器（controller）不能包含泛型",
-                        category: typeof(HttpServiceInvokerGenerator).FullName!,
-                        defaultSeverity: DiagnosticSeverity.Error,
-                        isEnabledByDefault: true), location);
+        private static readonly DiagnosticDescriptor Wag00004DiagDescriptor = new DiagnosticDescriptor(
+                id: "WAG00004",
+                title: "控制器（controller）不能包含泛型",
+                messageFormat: "控制器（controller）不能包含泛型",
+                category: typeof(HttpServiceInvokerGenerator).FullName!,
+                defaultSeverity: DiagnosticSeverity.Error,
+                isEnabledByDefault: true);
 
-        /// <summary>
-        /// 仅支持异步方法
-        /// </summary>
-        /// <param name="location"></param>
-        /// <returns></returns>
-        public static Diagnostic WAG00005(Location? location) => Diagnostic.Create(new DiagnosticDescriptor(
-                        id: "WAG00005",
-                        title: "仅支持异步方法",
-                        messageFormat: "仅支持异步方法",
-                        category: typeof(HttpServiceInvokerGenerator).FullName!,
-                        defaultSeverity: DiagnosticSeverity.Error,
-                        isEnabledByDefault: true), location);
+        private static readonly DiagnosticDescriptor Wag00005DiagDescriptor = new DiagnosticDescriptor(
+                id: "WAG00005",
+                title: "仅支持异步方法",
+                messageFormat: "仅支持异步方法",
+                category: typeof(HttpServiceInvokerGenerator).FullName!,
+                defaultSeverity: DiagnosticSeverity.Error,
+                isEnabledByDefault: true);
 
-        /// <summary>
-        /// 路由参数
-        /// </summary>
-        /// <param name="location"></param>
-        /// <returns></returns>
-        public static Diagnostic WAG00006(Location? location, string? symbolString = null) => Diagnostic.Create(new DiagnosticDescriptor(
-                        id: "WAG00006",
-                        title: "路由中未包含路由参数",
-                        messageFormat: $"路由中未包含路由参数({symbolString})",
-                        category: typeof(HttpServiceInvokerGenerator).FullName!,
-                        defaultSeverity: DiagnosticSeverity.Error,
-                        isEnabledByDefault: true), location);
+        private static readonly DiagnosticDescriptor Wag00006DiagDescriptor = new DiagnosticDescriptor(
+                id: "WAG00006",
+                title: "路由中未包含路由参数",
+                messageFormat: "路由中未包含路由参数({0})",
+                category: typeof(HttpServiceInvokerGenerator).FullName!,
+                defaultSeverity: DiagnosticSeverity.Error,
+                isEnabledByDefault: true);
 
-        /// <summary>
-        /// 不能同时设置[FromBody]和[FromForm]
-        /// </summary>
-        /// <param name="location"></param>
-        /// <returns></returns>
-        public static Diagnostic WAG00007(Location? location, string? symbolString = null) => Diagnostic.Create(new DiagnosticDescriptor(
-                        id: "WAG00007",
-                        title: "不能同时设置[FromBody]和[FromForm]",
-                        messageFormat: $"不能同时设置[FromBody]和[FromForm]({symbolString})",
-                        category: typeof(HttpServiceInvokerGenerator).FullName!,
-                        defaultSeverity: DiagnosticSeverity.Error,
-                        isEnabledByDefault: true), location);
+        private static readonly DiagnosticDescriptor Wag00007DiagDescriptor = new DiagnosticDescriptor(
+                id: "WAG00007",
+                title: "不能同时设置[FromBody]和[FromForm]",
+                messageFormat: "不能同时设置[FromBody]和[FromForm]({0})",
+                category: typeof(HttpServiceInvokerGenerator).FullName!,
+                defaultSeverity: DiagnosticSeverity.Error,
+                isEnabledByDefault: true);
 
-        /// <summary>
-        /// 不能设置多个[FromBody]
-        /// </summary>
-        /// <param name="location"></param>
-        /// <returns></returns>
-        public static Diagnostic WAG00008(Location? location, string? symbolString = null) => Diagnostic.Create(new DiagnosticDescriptor(
-                        id: "WAG00008",
-                        title: "不能设置多个[FromBody]",
-                        messageFormat: $"不能设置多个[FromBody]({symbolString})",
-                        category: typeof(HttpServiceInvokerGenerator).FullName!,
-                        defaultSeverity: DiagnosticSeverity.Error,
-                        isEnabledByDefault: true), location);
+        private static readonly DiagnosticDescriptor Wag00008DiagDescriptor = new DiagnosticDescriptor(
+                id: "WAG00008",
+                title: "不能设置多个[FromBody]",
+                messageFormat: "不能设置多个[FromBody]({0})",
+                category: typeof(HttpServiceInvokerGenerator).FullName!,
+                defaultSeverity: DiagnosticSeverity.Error,
+                isEnabledByDefault: true);
 
-        /// <summary>
-        /// 暂不支持的返回值类型
-        /// </summary>
-        /// <param name="location"></param>
-        /// <returns></returns>
-        public static Diagnostic WAG00009(Location? location, string? symbolString = null) => Diagnostic.Create(new DiagnosticDescriptor(
-                        id: "WAG00009",
-                        title: "暂不支持的返回值类型",
-                        messageFormat: $"暂不支持的返回值类型({symbolString})",
-                        category: typeof(HttpServiceInvokerGenerator).FullName!,
-                        defaultSeverity: DiagnosticSeverity.Error,
-                        isEnabledByDefault: true), location);
+
+        private static readonly DiagnosticDescriptor Wag00009DiagDescriptor = new DiagnosticDescriptor(
+                id: "WAG00009",
+                title: "暂不支持的返回值类型",
+                messageFormat: "暂不支持的返回值类型({0})",
+                category: typeof(HttpServiceInvokerGenerator).FullName!,
+                defaultSeverity: DiagnosticSeverity.Error,
+                isEnabledByDefault: true);
+
+        public static Diagnostic WAG00001(Location? location)
+        {
+            return Diagnostic.Create(Wag00001DiagDescriptor, location);
+        }
+
+        public static Diagnostic WAG00002(Location? location)
+        {
+            return Diagnostic.Create(Wag00002DiagDescriptor, location);
+        }
+
+        public static Diagnostic WAG00003(Location? location)
+        {
+            return Diagnostic.Create(Wag00003DiagDescriptor, location);
+        }
+
+        public static Diagnostic WAG00004(Location? location, string? symbolString = null)
+        {
+            return Diagnostic.Create(Wag00004DiagDescriptor, location, symbolString);
+        }
+
+        public static Diagnostic WAG00005(Location? location, string? symbolString = null)
+        {
+            return Diagnostic.Create(Wag00005DiagDescriptor, location, symbolString);
+        }
+
+        public static Diagnostic WAG00006(Location? location, string? symbolString = null)
+        {
+            return Diagnostic.Create(Wag00006DiagDescriptor, location, symbolString);
+        }
+
+        public static Diagnostic WAG00007(Location? location, string? symbolString = null)
+        {
+            return Diagnostic.Create(Wag00007DiagDescriptor, location, symbolString);
+        }
+
+        public static Diagnostic WAG00008(Location? location, string? symbolString = null)
+        {
+            return Diagnostic.Create(Wag00008DiagDescriptor, location, symbolString);
+        }
+
+        public static Diagnostic WAG00009(Location? location, string? symbolString = null)
+        {
+            return Diagnostic.Create(Wag00009DiagDescriptor, location, symbolString);
+        }
     }
 }
