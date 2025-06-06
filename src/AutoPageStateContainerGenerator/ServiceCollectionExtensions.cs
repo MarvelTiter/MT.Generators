f9353462c62dc9f329146f11869bddbd5268ead9
MarvelTiter <outlinmt@outlook.com>
2025-06-06 16:41:26 +0800

    添加功能

--- src/AutoPageStateContainerGenerator/ServiceCollectionExtensions.cs
+++ src/AutoPageStateContainerGenerator/ServiceCollectionExtensions.cs
@@ -11,26 +11,43 @@ namespace AutoPageStateContainerGenerator;
 /// <summary>
 /// 
 /// </summary>
+public class StateContainerOption
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public ServiceLifetime InjectLifetime { get; set; } = ServiceLifetime.Scoped;
+}
+
+/// <summary>
+/// 
+/// </summary>
 public static class ServiceCollectionExtensions
 {
     /// <summary>
     /// 
     /// </summary>
     /// <param name="services"></param>
+    /// <param name="config"></param>
     /// <returns></returns>
-    public static IServiceCollection AddStateContainers(this IServiceCollection services)
+    public static IServiceCollection AddStateContainers(this IServiceCollection services, Action<StateContainerOption>? config = null)
     {
         var entry = Assembly.GetEntryAssembly();
         if (entry is null) return services;
         var refereaces = entry.GetReferencedAssemblies().Select(Assembly.Load);
         Assembly[] all = [entry, .. refereaces];
+
+        var option = new StateContainerOption();
+        config?.Invoke(option);
+
         foreach (var asm in all)
         {
             foreach (var type in asm.ExportedTypes)
             {
                 if (type.GetCustomAttribute<GeneratedStateContainerAttribute>() == null)
                     continue;
-                services.AddScoped(type);
+                //services.AddScoped(type);
+                services.Add(new ServiceDescriptor(type, type, option.InjectLifetime));
             }
         }
         return services;
