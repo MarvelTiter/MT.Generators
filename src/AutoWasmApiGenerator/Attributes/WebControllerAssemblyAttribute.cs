abf22e3f5f95a610be0532579ee306d35fe28f7e
MarvelTiter <outlinmt@outlook.com>
2026-04-23 10:23:52 +0800

    备份

--- src/AutoWasmApiGenerator/Attributes/WebControllerAssemblyAttribute.cs
+++ src/AutoWasmApiGenerator/Attributes/WebControllerAssemblyAttribute.cs
@@ -8,5 +8,22 @@ namespace AutoWasmApiGenerator;
 [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = false)]
 public class WebControllerAssemblyAttribute : Attribute
 {
-
+    /// <summary>
+    /// 默认生成模式
+    /// </summary>
+    public enum ApiMode
+    {
+        /// <summary>
+        /// 生成Minimal API
+        /// </summary>
+        MinimalApi,
+        /// <summary>
+        /// 生成Controller
+        /// </summary>
+        Controller,
+    }
+    /// <summary>
+    /// Api生成模式
+    /// </summary>
+    public ApiMode Mode { get; set; } = ApiMode.Controller;
 }
